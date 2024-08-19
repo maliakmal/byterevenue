4c155fd96fc217ab39f49b2a4f77defb698daf75
pjovanic73 <p.jovanic73@gmail.com>
2024-08-19 11:24:43 -0700

    fornt-end for regenerate unsent

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -48,7 +48,7 @@
         <div class="p-6 m-6">
           <div class="p-6 m-6">
             <div class="p-6 m-6">
-              <div class="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">{{$params['total_not_downloaded_in_queue']}} / {{$params['total_in_queue']}}</div>                
+              <div class="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">{{$params['total_not_downloaded_in_queue']}} / {{$params['total_in_queue']}}</div>
               <small>Messages in Queue</small>
             </div>
           </div>
@@ -72,7 +72,7 @@
               <div class="mb-4 mt-4">
                 <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Short Domains</label>
                 <select id="url_shortener" name="url_shortener" class="shadow appearance-none border rounded w-half py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" >
-                @foreach($params['urlShorteners'] as $vv)  
+                @foreach($params['urlShorteners'] as $vv)
                 <option value="{{ $vv->name }}">{{ $vv->name }} {{ $vv->campaignShortUrls()->count() == 0 ? '(unused)': '('.$vv->campaignShortUrls()->count().' Camps.)' }}</option>
                   @endforeach
                 </select>
@@ -82,7 +82,7 @@
           <br/>
           </div>
           </div>
-      
+
       <div class="mt-5 bg-white overflow-hidden shadow-xl sm:rounded-lg">
         <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
 
@@ -93,13 +93,14 @@
             <th class="px-4 py-2">Filename</th>
             <th class="px-4 py-2">No. Entries</th>
             <th class="px-4 py-2">Created At</th>
+            <th class="px-4 py-2">Operation</th>
           </tr>
         </thead>
         <tbody>
             @foreach ($params['files'] as $file)
               <tr>
                 <td class="border-b border-gray-200 px-4 py-2">
-                  <a href="/download/{{$file['id'] }}">{{ $file['filename'] }}</a> 
+                  <a href="/download/{{$file['id'] }}">{{ $file['filename'] }}</a>
                 </td>
                 <td class="border-b border-gray-200 px-4 py-2">
                 {{ $file['number_of_entries'] }}
@@ -110,7 +111,14 @@
                     (pending)
                   @endif
                     {{ $file['created_at']->diffForHumans() }}
-                  </a> 
+                  </a>
+                </td>
+                <td class="border-b border-gray-200 px-4 py-2">
+                    <div class="inline-flex">
+                        <button data-modal-target="default-modal" data-modal-toggle="default-modal" style="width: 100px;font-size:13px;" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 rounded-l">
+                            Regenerate Unsent
+                        </button>
+                    </div>
                 </td>
               </tr>
             @endforeach
@@ -125,6 +133,41 @@
   </div>
 </x-app-layout>
 
+<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Terms of Service
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5 space-y-4">
+                <div class="form-group">
+                    <label>domain: </label>
+                    <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                        @foreach($params['urlShorteners'] as $vv)
+                            <option value="{{ $vv->name }}">{{ $vv->name }} {{ $vv->campaignShortUrls()->count() == 0 ? '(unused)': '('.$vv->campaignShortUrls()->count().' Camps.)' }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <!-- Modal footer -->
+            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-hide="default-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+            </div>
+        </div>
+    </div>
+</div>
 @push('scripts')
 <script>
     @if($params['download_me']!= null)
