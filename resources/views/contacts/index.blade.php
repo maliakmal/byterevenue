0fa7492a4863bbb4271076d2b6ba3932275c1010
pjovanic73 <p.jovanic73@gmail.com>
2024-08-27 11:39:10 -0700

    fix detail button

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -14,12 +14,12 @@
                   <svg class="h-5 w-5 flex-shrink-0 text-gray-400" x-description="Heroicon name: mini/chevron-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"></path>
                   </svg>
-                  <a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Contacts</a>
+                  <a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Data-Source</a>
                 </div>
               </li>
             </ol>
           </nav>
-          <h1 class="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Contacts</h1>
+          <h1 class="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Data-Source</h1>
           <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-8">
           </div>
         </div>
@@ -153,7 +153,7 @@
                 <span class="text-sm text-gray-500 dark:text-gray-400">${ele.email}</span>
                 <div class="flex mt-4 md:mt-6">
                     <a href="${edit}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-green-800">
-                        More Details
+                        Details
                     </a>
 
 
