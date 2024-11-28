1249589d47d60050d8ecf0c235b05be80c9e5ad9
Serhii V <serhii.voronovych@gmail.com>
2024-11-28 15:43:04 +0200

    added requestLogs

--- app/Services/Keitaro/KeitaroCaller.php
+++ app/Services/Keitaro/KeitaroCaller.php
@@ -2,8 +2,10 @@
 
 namespace App\Services\Keitaro;
 
+use App\Models\RequestLog;
 use App\Services\Keitaro\Requests\AbstractRequest;
 use Illuminate\Support\Facades\Http;
+use App\Models\Log;
 
 class KeitaroCaller
 {
@@ -13,9 +15,30 @@ class KeitaroCaller
         $path = $request->getPath();
         $headers = $request->getRequestHeader();
         $body = $request->getRequestBody();
-        $response = Http::retry(3)->withHeaders($headers)
-            ->$method($path, $body);
-        $response->throw();
-        return $response->json();
+
+        $log = new RequestLog();
+
+        $log->request = json_encode([
+            'method' => $method,
+            'path' => $path,
+            'headers' => $headers,
+            'body' => $body,
+        ]);
+
+        try {
+            $response = Http::retry(3)->withHeaders($headers)
+                ->$method($path, $body);
+            $response->throw();
+
+            $log->response = json_encode($response->json());
+            $log->status_code = $response->status();
+
+        } catch (\Exception $e) {
+            $log->exception = $e->getMessage();
+        }
+
+        $log->save();
+
+        return isset($response) ? $response->json() : json_encode([]);
     }
 }
---
+++ database/migrations/2024_11_28_132016_create_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('request_logs', function (Blueprint $table) {
+            $table->id();
+            $table->json('request')->nullable();
+            $table->json('response')->nullable();
+            $table->string('status_code')->nullable();
+            $table->string('url')->nullable();
+            $table->string('method')->nullable();
+            $table->longText('exception')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
