c9a76ce56e4bc42bf6e5a73ea584fbcd12832337
VS <serhii.voronovych@gmail.com>
2024-12-13 10:39:41 +0200

    change column

--- app/Console/Commands/tmpTest.php
+++ app/Console/Commands/tmpTest.php
@@ -36,9 +36,9 @@ class tmpTest extends Command
      */
     public function handle()
     {
-        \DB::table('broadcast_logs')->where('id', '>', 7)
-            ->where('id', '<', 12)
-            ->update(['id' => 7]);
+        \DB::table('broadcast_logs')->where('message_id', '>', 7)
+            ->where('message_id', '<', 12)
+            ->update(['message_id' => 7]);
 
         dd('completed');
     }
