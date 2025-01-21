fcae5dc237e6f5dafc98f2dc0556ab298129d197
VS <user@example.com>
2025-01-21 16:43:16 +0200

    tmp

---
+++ app/Events/PublicEvent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PublicEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel("public-event-channel"),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return array(
+            'message' => __('test message'),
+        );
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'public.notification';
+    }
+}
